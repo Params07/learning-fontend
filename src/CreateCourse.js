36950cad1710f8ffbb5c16367aa84a9122b085c2
Params07 <92327167+Params07@users.noreply.github.com>
2021-11-09 00:32:42 +0530

    Update CreateCourse.js

--- src/CreateCourse.js
+++ src/CreateCourse.js
@@ -79,9 +79,9 @@ const CreateCourse = ()=>
           
           
           
-          <div className=" pl-4 ... sm:p-8  mt-8 ... pt-8 ... grid sm:justify-items-center ...   ">
-              <div className="p-2 sm:p-4 md:3/5 sm:3/4  lg:w-2/5 w-80 grid justify-items-center ...  drop-shadow-lg rounded-lg bg-white ">
-              <div className="p-3 pt-6 ...  text-2xl font-semibold text-green-500 text-center ">
+          <div className="  sm:p-8  mt-8 ... pt-8 ... grid sm:justify-items-center ...   ">
+              <div className="p-0 sm:p-4 md:3/5 sm:3/4  lg:w-2/5 w-80 grid justify-items-center ...  rounded-lg bg-white ">
+              <div className="p-6 text-2xl font-semibold text-green-500 text-center ">
               CREATE NEW COURSE
           </div>
           
@@ -149,7 +149,7 @@ const CreateCourse = ()=>
                             </Listbox>
     </div>
     <div className="pt-8 ...">
-                      <textarea  required onChange={updateDes}  value={description} rows="5" cols="25" className=" px-3 block border-2 border-green-300  focus:outline-none focus:border-green-600 focus-ring-2 focus-ring-green-500 rounded-lg leading-6" placeholder="enter the description"></textarea>
+                      <textarea  required onChange={updateDes}  value={description} rows="5" cols="28" className=" px-3 block border-2 border-green-300  focus:outline-none focus:border-green-600 focus-ring-2 focus-ring-green-500 rounded-lg leading-6" placeholder="enter the description"></textarea>
                   </div>
                   <div className="pt-8 ... ">
                       <input  required type="url" onChange={updateUrl}  value={url} className="py-2 w-64 sm:w-72 px-3 block border-2 border-green-400  focus:outline-none focus:border-green-600 focus-ring-2 focus-ring-green-500 rounded-lg leading-6" placeholder="enter the URL"/>
