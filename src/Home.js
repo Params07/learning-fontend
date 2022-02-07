431741db400a14769c89a9e1bc37a582295f9f36
Params07 <92327167+Params07@users.noreply.github.com>
2022-02-08 01:12:23 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,54 @@
-import React from 'react'
+
+
+import {React,useState,useEffect} from 'react';
+import Content from './Content'
 
 
 
-const Home =()=>
-{
+
+
+
+function Home() {
+ const [open,setopen] = useState(false);
+ 
+
   return(
     <>
-        <div className="p-8 text-2xl text-white font-black text-center">
-          <p className="p-8">LEARNING IS EVERYTHING</p>
-         
+    <>
+    <div className={`pt-8 ... ${!open ? "block":"hidden"}`}>
+      <div className=" pt-8 ... sm:pr-8 ... sm:pl-8 ... flex items-center justify-center ">
+        <div className="p-0 sm:p-8  w-full xl:w-4/5 h-full xl:h-screen  bg-white md:shadow-lg rounded-lg ... xl:flex grid">
+        <div className=" xl:p-0 sm:p-8 p-6 w-full xl:w-1/2 ">
+                   <img src="home.jpeg" className=" rounded w-full h-full" alt="Oops"/>
+               </div>
+               <div className=" p-4 sm:p-2 xl:p-6 w-full xl:w-1/2 h-full  text-2xl sm:flex  sm:justify-center ">
+                   <div className="w-full sm:w-3/4">
+                       <span className="text-3xl font-black">
+                          Your Course to Success
+                       </span>      
+                       <br>
+                       </br>
+                       <span className="text-lg font-semibold">
+                         Build your skills with programming courses 
+                       </span>
+                       <br>
+                       </br>
+                       <br>
+                       </br>
+                       <div className="">
+                          <button onClick={(e)=>{setopen(true)}} className="  h-10 p-1 sm:h-14 sm:p-2  rounded border-2 border-green-300  hover:bg-white hover:text-green-300  bg-green-300 text-white">Learn courses</button>
+                         </div>
+                       
+                   </div>
+                   </div>
         </div>
+      </div>
+    </div>
+
+    <div className={`bg-green-300 ${open ? "block":"hidden"}`}>
+      <Content/>
+    </div>
+    </>
 
     </>
   );
